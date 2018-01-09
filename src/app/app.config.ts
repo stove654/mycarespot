a4c6fb5e897050117ccc08f0c0a0d50e22cbce12
Loi Hoang <imstart25@gmail.com>
2018-01-09 20:52:10 +0700

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 
 export const Config = {
-  url: 'http://192.168.10.106:8080', //174.138.88.121
+  url: 'http://192.168.13.100:8080', //174.138.88.121
   oneSignalId: '0aefe444-ad09-494a-b457-2d9febdbf78e',
   api: {
     login: '/auth/phone/',
